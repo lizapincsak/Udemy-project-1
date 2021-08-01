bb9c3a4a4e95340c69104e0a0e32b0898fbf5674
lizapincsak <liza.pincsak@gmail.com>
2021-08-01 14:20:51 -0500

    conditional statements

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,8 +1,8 @@
 import React, { useState } from 'react';
-import ExpenseItem from "./ExpenseItem";
 import Card from '../UI/Card';
 import './Expenses.css';
 import ExpenseFilter from './ExpenseFilter';
+import ExpensesList from './ExpensesList';
 
 const Expenses = props => {
 
@@ -12,17 +12,18 @@ const Expenses = props => {
     setFilteredYear(selectedYear)
   }
 
+  const filteredExpenses = props.items.filter(expense => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  })
+
     return (
       <div>
         <Card className="expenses">
-        <ExpenseFilter selected={filteredYear} onChangeFilter={filterChangeHandler}/>
-          {props.items.map((expense) => (
-            <ExpenseItem 
-              title={expense.title} 
-              amount={expense.amount} 
-              date={expense.date}
-            />
-          ))}
+        <ExpenseFilter 
+          selected={filteredYear} 
+          onChangeFilter={filterChangeHandler}
+        />
+        <ExpensesList items={filteredExpenses}/>
         </Card>
       </div>
     )
